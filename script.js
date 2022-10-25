017a3e6fdd6138765aa6290c96c943eba9e6adef
aidanespinosa <aidanespinosa3@gmail.com>
2022-10-24 20:13:22 -0700

    set css layout

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+const apiToken = c4277b2a11msh036137154db892dp146f21jsna944452c6882;
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "c4277b2a11msh036137154db892dp146f21jsna944452c6882",
+    "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+  },
+};
+
+const params = {
+  query: ingredient,
+};
