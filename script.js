b2eea5125cbdc6110bf8264ddcad8566e9dd9266
tnguyen101197 <t.nguyen101197@gmail.com>
2022-10-25 20:13:05 -0700

    connect to spoonacular AP

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-"use strict";
-const apiToken = c4277b2a11msh036137154db892dp146f21jsna944452c6882;
-let ingredient;
-function getRecipe(ingredient) {
+// var apiURL = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=${ingredient}`;
+
+var button = document.querySelector('.button');
+
+button.addEventListener ('click', function () {
+  let ingredient = document.querySelector('#ingredient').value.trim(); 
+  
+})
+
+
+
+var ingredientData = function(ingredient) {
   const options = {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "X-RapidAPI-Key": "c4277b2a11msh036137154db892dp146f21jsna944452c6882",
-      "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-    },
+      'X-RapidAPI-Key': '43db8d39ccmsheb00f22c656a258p1ea5b5jsnec5c39455c0f',
+		  'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+    }
   };
-
-  //   const params = {
-  //     query: ingredient,
-  //   };
-
-  return fetch(
-    `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=${pasta}`,
-    options
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      ingredient = data.ingredient;
-    })
-    .catch((err) => console.error(err));
+  fetch (`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=${ingredient}`, options) 
+  .then(response => response.json())
+  .then(response => console.log(response))
+	.catch(err => console.error(err));  
 }
-getRecipe();
+
+
+ingredientData('shrimp');
